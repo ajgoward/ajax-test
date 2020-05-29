46600bf23a9c4309dcbb928e3ec9e476ceed8033
ajgoward <ajgoward@gmail.com>
2020-05-29 14:16:33 +0000

    completed ajax text displaying star wars in a table format using js

--- main.js
+++ main.js
@@ -1,40 +1,47 @@
-const baseURL = "https://ci-swapi.herokuapp.com/api/"
-
-function getData(type, cb) {
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", baseURL + type + "/" );
-    xhr.send();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
+
+    xhr.open("GET", url);
+    xhr.send();
 }
-
-//function printDataToConsole(data) {//
-   // console.log(data);//
-//}//
-
-//getData(printDataToConsole);//  // All this prints the data to the console////
 
 function getTableHeaders(obj) {
     var tableHeaders = [];
 
     Object.keys(obj).forEach(function(key) {
-        tableHeaders.push(`<td>${key}</td>`)
+        tableHeaders.push(`<td>${key}</td>`);
     });
 
     return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
-    var el = document.getElementById("data");
-    el.innerHTML = "";
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
 
-    getData(type, function(data) {
-        var tableRows = [];
+function writeToDocument(url) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+
+    getData(url, function(data) {
+        var pagination = "";
+
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous);
+        }
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
@@ -46,9 +53,9 @@ function writeToDocument(type) {
                 var truncatedData = rowData.substring(0, 15);
                 dataRow.push(`<td>${truncatedData}</td>`);
             });
-            tableRows.push(`<tr>${dataRow}</tr>`)
+            tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
